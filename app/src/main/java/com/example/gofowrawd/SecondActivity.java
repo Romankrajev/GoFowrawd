d59f3e30770dcbc6cc204a782ec330a653ea098b
Romal <roman.krajjev@gmail.com>
2022-06-19 09:46:20 +0300

    First commit

---
+++ app/src/main/java/com/example/gofowrawd/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.gofowrawd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void goThird(View view) {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+
+    }
+
+}
