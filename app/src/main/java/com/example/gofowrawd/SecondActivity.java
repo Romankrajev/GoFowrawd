3f7788c8c779852b0ae4590ecfc98d903f15f79c
Romal <roman.krajjev@gmail.com>
2022-06-19 09:50:24 +0300

    Second Page View was added

--- app/src/main/java/com/example/gofowrawd/SecondActivity.java
+++ app/src/main/java/com/example/gofowrawd/SecondActivity.java
@@ -3,15 +3,22 @@ package com.example.gofowrawd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    public static final String PREFS_NAME = "SharedPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        SharedPreferences storedData = getSharedPreferences(PREFS_NAME, 0);
+        String userName = storedData.getString("userName", null);
+        TextView textView = findViewById(R.id.username);
+        textView.setText(userName);
     }
 
     public void goThird(View view) {
